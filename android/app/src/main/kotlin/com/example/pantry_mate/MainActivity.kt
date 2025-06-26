6df93d09a7a4dc25ccc8233bc11b2c0fdc81b21f
Sebastian Lembcke <sebastiankarel.lembcke@googlemail.com>
2025-06-26 21:04:06 +0200

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/pantry_mate/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pantry_mate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Pantry Mate",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
